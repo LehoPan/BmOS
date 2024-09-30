6fc9427761167b7a2595fcf33098060e54aead70
Leo Pan <leo.long.pan@gmail.com>
2024-09-30 19:52:50 +0000

    audio test

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,8 @@ int main(){
     BmOS hiii;
 
     hiii.render();
+    system("ffplay -t '5' -autoexit ./sfx/LocalForecast-Elevator.wav &");
+
     while(true){
         hiii.play();
         usleep(700000);
